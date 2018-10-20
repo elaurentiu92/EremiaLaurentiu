d528f85615d6ebde3e6c1761d6cf41e689044efc
elsawhere <elsawhere@eremias-air>
2018-10-20 18:31:33 +0300

    Regex to validate CNP

---
+++ Java9EremiaLaurentiu/src/regex/RegularExpression.java
@@ -0,0 +1,36 @@
+/*
+ * Creating a pattern for identifying CNP
+ */
+package regex;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Scanner;
+/**
+ *
+ * @author Laurentiu
+ */
+public class RegularExpression {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        System.out.println("Input the pattern: ");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.next();
+        String regex = "^[1-2](0\\d|1[012])(0\\d|1\\d|2\\d|3[0-1])(19\\d\\d|20\\d\\d)\\d{4}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(userInput);
+        
+        if(matcher.find()){
+            System.out.println("That's a valid CNP");
+        }
+        else{
+            System.out.println("That's not a valid CNP");
+        }
+        
+        
+    }
+    
+}
